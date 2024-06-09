a716b8c23c87c1e634dcb9a56b439994c4f65526
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-09 11:06:03 +0530

    new commit

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div><h1>About</h1><br></br><p>I am here at Jhalwa Voice</p></div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
+// import {BrowserRouter as Router,Routes,Route} form "react-router-dom";
+import {BrowserRouter as Router,Routes,Route,Link}from "react-router-dom"
+import Switch from "./Switch";
 
 const App = () => {
   return (
+    
+    <Router>
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+       <li> <Link to={'/'}>Home</Link></li>
+        <li><Link to={'/about'}>About</Link></li>
+        </ul>
     </div>
+    <Switch />
+    </Router>
+    
+  
   )
 }
 
 export default App
+
+
+
